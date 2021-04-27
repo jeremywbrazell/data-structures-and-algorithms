4c73c64773ea594d2f0c2139c0e3b350da5dc942
jeremywbrazell <jwbrazell@gmail.com>
2021-04-27 14:57:29 -0700

    pushing unfinished cc7

---
+++ javascript/code-challenges/ll-kth-from-end/ll.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const Node = require('./node.js');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    // instantiate a new node to add to our LL
+    let node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      // traversal happens here
+      let current = this.head; // start the traversal at the beginning (head) of the LL
+
+      while (current.next) { // this is the actual traversal
+        current = current.next; // this moves you along in your traversal
+      }
+
+      current.next = node;
+    }
+
+    return this;
+  }
+  includes(val) {
+    let thisNode = this.head;
+
+    while (thisNode) {
+      if (thisNode.value === val) {
+        return thisNode;
+      }
+
+      thisNode = thisNode.next;
+    }
+
+    return thisNode;
+  }
+  insertBefore(val, newVal) {
+    let thisNode = this.head;
+
+    while (thisNode) {
+      if (thisNode.value === val) {
+        return thisNode;
+      }
+
+      thisNode = thisNode.next;
+    }
+
+    return thisNode;
+  }
+  insertAfter(val, newVal) {
+    // let thisNode = this.head;
+    let newNode = new Node(value);
+
+    while (newNode) {
+      if (thisNode.value === val) {
+        return thisNode;
+      }
+
+      thisNode = thisNode.next;
+    }
+
+    return thisNode;
+  }
+
+  kthFromEnd(k) {
+      let list = 
+    if (tail===null) {
+     return tail - k;
+    } else {
+    }}
+      
+}
+
+module.exports = LinkedList;
