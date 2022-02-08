9ffbf9d12f1ee551322feda051e3b6f5ba1f4006
jeremywbrazell <jwbrazell@gmail.com>
2022-02-07 20:34:18 -0800

    cc38

---
+++ 401-code-challenges/hashmap-left-join/hashmap-left.join.js
@@ -0,0 +1,11 @@
+const Hashmap = require('./hm.js');
+
+function leftJoin(hm1, hm2) {
+// let synonymHM = [];
+// let antonymHM = [];
+for (let index = 0; index < hm1.length; index++) {
+    
+    
+}
+
+}
